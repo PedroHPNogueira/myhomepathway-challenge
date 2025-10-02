0d5c92d08ebc55413df632e207284a76c100ff80
Pedro Nogueira <pedrophpn2002@hotmail.com>
2025-10-02 06:59:57 -0300

    feat: create omdb service

---
+++ backend/src/omdb/omdb.service.ts
@@ -0,0 +1,66 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import axios, { AxiosError, AxiosInstance } from 'axios';
+
+export interface OmdbMovie {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+
+export interface OmdbSearchResponse {
+  Search: OmdbMovie[];
+  totalResults: string;
+  Response: string;
+}
+
+export interface OmdbErrorResponse {
+  Error: string;
+  Response: string;
+}
+
+@Injectable()
+export class OmdbService {
+  private readonly axiosInstance: AxiosInstance;
+  private readonly apiKey: string;
+  private readonly logger = new Logger(OmdbService.name);
+
+  constructor() {
+    this.apiKey = process.env.OMDB_API_KEY || '';
+    console.log('apiKey', this.apiKey);
+    this.axiosInstance = axios.create({
+      baseURL: 'http://www.omdbapi.com/',
+      timeout: 10000,
+      params: {
+        apikey: this.apiKey,
+      },
+    });
+
+    this.axiosInstance.interceptors.response.use(
+      (response) => response,
+      (error: AxiosError) => {
+        if (error.response?.status === 401) {
+          this.logger.error('The OMDB API key is invalid or expired. Please check the OMDB_API_KEY configuration.');
+        }
+
+        throw new InternalServerErrorException('Error searching movies on OMDB');
+      },
+    );
+  }
+
+  async searchMovies(search: string, page = 1): Promise<OmdbSearchResponse> {
+    const response = await this.axiosInstance.get<OmdbSearchResponse | OmdbErrorResponse>('/', {
+      params: {
+        s: search,
+        page,
+      },
+    });
+
+    if ('Error' in response.data) {
+      throw new BadRequestException(response.data.Error);
+    }
+
+    return response.data;
+  }
+}
