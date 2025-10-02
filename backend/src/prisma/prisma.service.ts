898b6df64b0857fc80ca292d1fddd5ae002e2f26
Pedro Nogueira <pedrophpn2002@hotmail.com>
2025-10-02 08:09:52 -0300

    feat: create ts path for prima generated

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,6 +1,5 @@
+import { PrismaClient } from '@generated/prisma';
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-
-import { PrismaClient } from '../../generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
