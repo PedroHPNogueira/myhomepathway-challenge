3f6b0c49191be127649783ade0bf992c7d9c5990
Pedro Nogueira <pedrophpn2002@hotmail.com>
2025-10-02 05:32:18 -0300

    feat: configure eslint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { dirname } from 'path';
@@ -13,6 +16,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       globals: {
@@ -25,12 +29,22 @@ export default tseslint.config(
         tsconfigRootDir: dirname(fileURLToPath(import.meta.url)),
       },
     },
+    plugins: {
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
   },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': 'warn',
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+      'import/first': 'warn',
+      'import/newline-after-import': 'warn',
+      'import/no-duplicates': 'warn',
     },
   },
 );
